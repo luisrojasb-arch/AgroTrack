96e1bd9c39befe665f6cc5c400a7e235b360aac5
0smarcit0 Porras <112773178+0smarcit0@users.noreply.github.com>
2026-08-12 22:25:01 -0400

    added components for the alerts list

--- frontend/src/components/overview/OverviewList.jsx
+++ frontend/src/components/overview/OverviewList.jsx
@@ -1,14 +1,35 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import Pagination from "@/components/ui/Pagination";
+import AlertasContainer from "@/components/overview/AlertasContainer";
 
+export default function OverviewList({ data }) {
+  const searchParams = useSearchParams();
+  
+  const paginaActualUrl = Number(searchParams.get("page")) || data.paginacion.paginaActual || 1;
+  const limite = data.paginacion.limite || 5;
+  console.log("paginaActualUrl:", paginaActualUrl);
+  const startIndex = (paginaActualUrl - 1) * limite;
+  const endIndex = startIndex + limite;
+  
+  const alertasPaginadas = data.alertas.slice(startIndex, endIndex);
 
-export default function OverviewList({data}) {
+  return (
+    <div className="bg-white rounded-3xl border border-[var(--color-border-agro)] p-6 flex flex-col w-full h-full shadow-sm">
+      <div className="flex-1 w-full">
+        <AlertasContainer alertas={alertasPaginadas} />
+      </div>
 
-
-    return(
-        <div className="bg-white rounded-3xl border border-(--color-border-agro) p-6 flex flex-col w-full h-full shadow-sm">
-           <div className="flex flex-col items-center justify-center w-full gap-4 py-4">
-               <Pagination totalRegistros={3} totalPaginas={2} paginaActual={1} limite={3} />
-            </div>
-        </div>
-    );
+      <div className="mt-auto w-full">
+        <Pagination
+          totalRegistros={data.paginacion.totalRegistros}
+          totalPaginas={data.paginacion.totalPaginas}
+          paginaActual={paginaActualUrl} 
+          limite={limite}
+          compact={true}
+        />
+      </div>
+    </div>
+  );
 }
